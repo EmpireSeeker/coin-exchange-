3ec9a9d3762d14f6db77f96559383a4ed19419c1
Etom Usang <laiveetom@gmail.com>
2024-09-02 12:57:56 +0100

    Added a few methods to FrontController

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{Coin, Setting, Section}
 
 class FrontController extends Controller
 {
-    //
+    private $settings = Setting::all();
+    public function index()
+    {
+        return view('index', ['title' => 'Home - Welcome', 'coins' => Coin::all(), 'sections' => Section::all()]);
+    }
+
+    public function about()
+    {
+        return view('about', ['title' => 'About', 'settings' => Setting::all()]);
+    }
+
+    public function exchange(Coin $id)
+    {
+        return view('exchange', ['title' => 'Our rates', 'coins' => Coin::all()]);
+    }
+
+    public function support()
+    {
+        return view('support', ['title' => 'Support', 'settings' => Setting::all()]);
+    }
 }
